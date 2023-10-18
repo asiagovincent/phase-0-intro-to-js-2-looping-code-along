fc1fbfca7c8b4f89d2fcaa302488493b671bed5c
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-18 21:38:54 +0300

    done with the lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+  }
+  
+  function countDown(positiveInteger) {
+    while (positiveInteger >= 0) {
+      console.log(positiveInteger);
+      positiveInteger--;
+    }
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const thankYouCards = writeCards(names, eventName);
+  
+  console.log(thankYouCards);
+  
+  const countdownValue = 10;
+  countDown(countdownValue);
+  
